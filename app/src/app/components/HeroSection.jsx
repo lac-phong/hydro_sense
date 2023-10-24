9b5a03fc72742c43bd9806e41d9ee2bb32f88d64
hanbit0218 <han02bit18@gmail.com>
2023-10-23 18:53:45 -0700

    frontend commit

---
+++ app/src/app/components/HeroSection.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+import logo from 'public/logo.png';
+
+console.log(logo);
+
+const HeroSection = () => {
+    const imageStyle = {
+        width: '20px',
+        height: '20px',
+      };
+  
+    return (
+      
+    <div className="flex">
+      
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <div className="wp_text_container text-black">
+            <img src={logo} alt="Logo" />
+            <TypeAnimation
+              sequence={[
+                "HYDRO SENSE", 
+                30,
+                "STAY ONE STEP AHEAD", 
+                30
+              ]}
+
+              wrapper="span"
+              speed={1}
+              style={{fontSize: '0.8em', display: 'inline-block'}}
+              repeat={Infinity}
+            />
+          </div>
+          <h1 className="wp_text_container text-black mb-4 text-6xl font-bold">WELCOME</h1>
+          <h1 className="name_text_container text-[#BBC275] mb-4 text-6xl font-bold"><span className="gradient-text bg-clip-text bg-gradient-to-r from-green-600 to-green-300">NAME{" "}</span></h1>
+                
+          <div className="button-container">
+            <button className="custom-button">CREATE NEW SHEET</button>
+            <button className="custom-button">EDIT EXISTING SHEET</button>
+            <button className="custom-additional-button"><span className="about-text">ABOUT</span> <span className="garden-text">GARDEN</span></button>
+            <button className="custom-additional-button"><span className="about-text">EXISTIN SHEETS</span></button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
