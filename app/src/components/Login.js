2b50589d1ce9d34bc6c5026f1412c99af8963fdd
lac-phong <quanglac03@gmail.com>
2023-11-02 16:41:49 -0700

    fixed auth

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -4,6 +4,21 @@ import { GoogleLogin } from 'react-google-login';
 const clientId = "274329865046-8bmr8o2mtil4qr13ttj0gc8ln6v6u5va.apps.googleusercontent.com";
 
 export default function Login({setUserName}) {
+
+    useEffect(() => {
+        async function loadGapiScript() {
+          const gapi = await import('gapi-script').then((pack) => pack.gapi);
+          gapi.load('client:auth2', start);
+        }
+    
+        function start() {
+          gapi.client.init({
+            clientId: clientId,
+          });
+        }
+    
+        loadGapiScript();
+      }, [clientId]);
     
     const onSuccess = (res) => {
         console.log("LOGIN SUCCESS! Current user: ",res.profileObj);
@@ -22,7 +37,7 @@ export default function Login({setUserName}) {
                 onSuccess = {onSuccess}
                 onFailure = {onFailure}
                 cookiePolicy = {'single_host_origin'}
-                isSignedIn = {false}
+                isSignedIn = {true}
             />
         </div>
     )
