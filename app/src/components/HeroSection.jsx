5bc5a4a645e32d636a5b4fa2f7fef25a9885532a
juliahusainzada <juliahusainzada@gmail.com>
2023-11-05 19:22:13 -0800

    Hid/showed login/logout buttons

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -1,64 +1,76 @@
-import React, { useState, useContext, useEffect } from 'react'
-import Image from 'next/image'
+import React, { useState, useContext, useEffect } from 'react';
+import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
-import ExistingSheets from './ExistingSheets'
+import ExistingSheets from './ExistingSheets';
 import Login from './Login';
 import Logout from './Logout';
 import NameBar from './NameBar';
 import { SpreadsheetContext } from './SpreadsheetContext';
+import Link from 'next/link';
 
 const HeroSection = () => {
+  const [userName, setUserName] = useState('');
+  const { spreadsheetInfo } = useContext(SpreadsheetContext);
 
-    const [userName, setUserName] = useState('');
-    const { spreadsheetInfo } = useContext(SpreadsheetContext)
-
-    return (      
+  return (
     <div className="top">
       <div class="container">
         <div class="content">
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <div className="wp_text_container text-black">
-            <Image src="/logo.png" width={20} height={20} alt="Logo" />
-            <p>
-            <TypeAnimation
-              sequence={[
-                "HYDRO SENSE", 
-                30,
-                "STAY ONE STEP AHEAD", 
-                30
-              ]}
+          <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center">
+              <div className="wp_text_container text-black">
+                <Image src="/logo.png" width={20} height={20} alt="Logo" />
+                <p>
+                  <TypeAnimation
+                    sequence={[
+                      "HYDRO SENSE",
+                      30,
+                      "STAY ONE STEP AHEAD",
+                      30,
+                    ]}
+                    wrapper="span"
+                    speed={1}
+                    style={{ fontSize: '0.8em', display: 'inline-block' }}
+                    repeat={Infinity}
+                  />
+                </p>
+              </div>
+              <h1 className="wp_text_container text-black mb-4 text-6xl font-bold">
+                WELCOME
+              </h1>
+              {userName ? (
+                <div>
+                  <h1 className='name_text_container text-[#96BD7A] mb-4 text-6xl font-bold '>
+                    <span className='gradient-text bg-clip-text bg-gradient-to-r from-green-600 to-green-300'>
+                      {userName}
+                    </span>
+                  </h1>
+                  <Logout setUserName={setUserName} />
+                </div>
+              ) : (
+                <Login setUserName={setUserName} />
+              )}
 
-              wrapper="span"
-              speed={1}
-              style={{fontSize: '0.8em', display: 'inline-block'}}
-              repeat={Infinity}
-            />
-            </p>
-          </div>
-          <h1 className="wp_text_container text-black mb-4 text-6xl font-bold">WELCOME</h1>            
-          
-          <h1 className='name_text_container text-[#96BD7A] mb-4 text-6xl font-bold '><span className='gradient-text bg-clip-text bg-gradient-to-r from-green-600 to-green-300'>{userName}</span></h1>
-          <div className='button-container'>
-              <Login setUserName = {setUserName} />
-              <Logout setUserName = {setUserName} />
               <NameBar />
               <ExistingSheets />
-              <p className='text-black'>Spreadsheet name: {spreadsheetInfo.name}</p>
-              <p className='text-black'>Spreadsheet ID: {spreadsheetInfo.id}</p>
-              <a href='LINK OF ABOUT US PAGE'>
-                <button className='custom-additional-button'><span className='about-text'> ABOUT </span><span className='garden-text'>GARDEN</span></button>
-             </a> 
+              <Link href='/AboutPage'>
+                <button className='custom-additional-button'>
+                  <span className='about-text'> ABOUT </span>
+                  <span className='garden-text'>GARDEN</span>
+                </button>
+              </Link>
+
+              <br />
+
+              <Link href='/MeetTheTeam'>
+                <button className='custom-additional-button'>ABOUT TEAM</button>
+              </Link>
+            </div>
           </div>
         </div>
-        
       </div>
-
-        </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
