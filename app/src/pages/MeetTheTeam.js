4e9cfde154c8000acee123144592980cc5126a0a
hanbit0218 <han02bit18@gmail.com>
2023-11-05 20:48:56 -0800

    formatting

--- app/src/pages/MeetTheTeam.js
+++ app/src/pages/MeetTheTeam.js
@@ -5,78 +5,72 @@ import Image from 'next/image'
 const MeetTheTeam = () => {
   return (
     <nav>
-        <h1 className="flex text-base md:text-5xl lg:text-6xl font-bold">HYDRO SENSE PROJECT AND TEAM</h1>
+      <div className="bg-gray-100 p-4 ml-5 mt-5 mr-5 rounded-lg">
+        <h1 className="text-base sm:text-3.5xl md:text-4xl lg:text-4.5xl font-bold text-center">MEET THE TEAM</h1>
+      </div>
 
-        <br/>
-       
-        <h3>ABOUT THE PROJECT</h3>
+      <div className="bg-gray-100 p-4 ml-5 mt-5 mr-5 rounded-lg">
+        <h1 className="flex text-base md:text-2xl lg:text-3xl font-bold mb-2">ABOUT THE PROJECT</h1>
         <u1>
-            <li>The object of the Hydro Sense Project was to help out SJSU community garden to optimize its water usage</li>
-            <li>We wanted to achieve this by automating the process of recording the moisture and temperature data throughout the day for the garden to develop better watering practices and systems</li>
+          <li className="ml-9">The object of the Hydro Sense Project was to help out <span className="underline font-semibold">SJSU Community Garden</span> to <span className="underline font-semibold">optimize</span> its water usage</li>
+          <li className="ml-9">We wanted to achieve this by automating the process of recording the moisture and temperature data throughout the day for the garden to develop better watering practices and systems</li>
         </u1>
-        <br/>
-        <br/>
-        <h3>ABOUT THE TEAM</h3>
-
-    
-      <div> 
-      <u1> 
-        <li>The teams for this project consists of 17 Fall '23 Potential New Members of the San Jose State University Theta Tau Professional Engineering Fraternity</li>
-      <li>The members were split into four teams, including the hardware, microcontroller, frontend, and backend teams</li>
-      </u1> 
-
-      <br/>
-      Here are the members of all the teams!
-      <u1> 
-      <li>Hardware Team: Chris Wong, Justin Doan, Kevin Bautista, Janelle Bautista, Manan Upadhyay, Ethan Nyugen, Sid Nayak</li>
-      <li>Microcontroller Team: James Kim, Michelle Kim, Drew Tom</li>
-      <li>Frontend Team: David Song, Alan Wang, Krishiv Haranath</li>
-      <li>Backend Team: Lac-Phong Nyugen, Stefan Le, Julia Husainzada, Akshitaa Balasai</li>
-      </u1> 
-      <br/>
-      <br/>
+      </div>
       
+      <div className="flex flex-col lg:flex-row">
+        <div className="bg-gray-100 p-4 ml-5 mt-5 mr-5 rounded-lg items-stretch">
+          <h1 className="flex text-base md:text-2xl lg:text-3xl font-bold mb-2">THE TEAMS</h1>
+          <u1> 
+            <li className="ml-9">The teams for this project consists of <span className="underline font-semibold">17 Fall '23 Potential New Members of the San Jose State University Theta Tau Professional Engineering Fraternity</span></li>
+            <li className="ml-9">The members were split into four teams, including the HARDWARE, MICROCONTROLLER, FRONTEND, and BACKEND teams</li>
+          </u1>
+        </div>
+
+        <div className="bg-gray-100 p-4 ml-5 mt-5 mr-5 mb-5 rounded-lg items-stretch">
+          <h1 className="flex text-base md:text-2xl lg:text-3xl font-bold mb-2">TEAM MEMBERS</h1>
+          <u1 className="mb-4"> 
+            <li className="ml-9"><span className="underline font-semibold">Hardware Team</span>: Chris Wong, Justin Doan, Kevin Bautista, Janelle Bautista, Manan Upadhyay, Ethan Nyugen, Sid Nayak</li>
+            <li className="ml-9"><span className="underline font-semibold">Microcontroller Team</span>: James Kim, Michelle Kim, Drew Tom</li>
+            <li className="ml-9"><span className="underline font-semibold">Frontend Team</span>: David Song, Alan Wang, Krishiv Haranath</li>
+            <li className="ml-9"><span className="underline font-semibold">Backend Team</span>: Lac-Phong Nyugen, Stefan Le, Julia Husainzada, Akshitaa Balasai</li>
+          </u1>
+        </div>
       </div>
-      <Image src="/HydroSenseTeam.jpg" width={500} height={500} alt="Pic1"/>
-
-
-      <div className="flex justify-center items-center"> {/**/}
-      </div>
-
-      <div>
-
+      
+      <div className="flex bg-gray-100 p-4 ml-5 mt-5 mr-5 mb-5 rounded-lg items-center justify-center">
+        <Image src="/HydroSenseTeam.jpg" width={500} height={500} alt="Pic1"/>
       </div>
 
 
-      <Link href="https://discord.gg/X8PGQ5WAV" target="_blank" rel="noopener noreferrer">
-        <button  className="p-2 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:bg-blue-600 transition duration-300">
-        <Image src="/discord-logo.png" width={50} height={50} alt="Pic1"/>
-        </button>
-      </Link>
+      <div className="flex bg-gray-100 p-4 ml-5 mt-5 mr-5 mb-5 rounded-lg items-center justify-center">
+        <Link href="https://discord.gg/X8PGQ5WAV" target="_blank" rel="noopener noreferrer">
+          <button className="mr-5">
+          <Image src="/discord.png" width={50} height={50} alt="Pic1"/>
+          </button>
+        </Link>
 
-      <br />
+        <br />
 
-      <Link href="https://www.instagram.com/pnmf23/" target="_blank" rel="noopener noreferrer">
-        <button  className="p-2 bg-red-500 hover:bg-red-600 focus:outline-none focus:bg-red-600 transition duration-300">
-        <Image src="/insta-logo.png" width={50} height={50} alt="Pic1"/>
-        </button>
-      </Link>
+        <Link href="https://www.instagram.com/pnmf23/" target="_blank" rel="noopener noreferrer">
+          <button className="ml-5">
+            <Image src="/instagram_icon.png" width={50} height={50} alt="Pic1"/>
+          </button>
+        </Link>
+      </div>
 
-      <br />
+      <div className="flex bg-gray-100 p-4 ml-5 mt-5 mr-5 mb-5 rounded-lg items-center justify-center">
+        <Link href="/">
+          <button className="bn54 mr-5">
+            <span class="bn54span">Home</span>
+          </button>
+        </Link>
 
-      <Link href="/">
-        <button>
-          Home
-        </button>
-      </Link>
-
-      <br />
-
-      <Link href="/AboutPage">
-        <button>
-          About Garden
-        </button>
-      </Link>
+        <Link href="/AboutPage">
+        <button className="bn54 ml-5">
+            <span class="bn54span">About Garden</span>
+          </button>
+        </Link>
+      </div>
     </nav>
   )
 }
