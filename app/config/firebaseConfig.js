c66a42499a515828858252b9a1ceba8ee0192473
lac-phong <quanglac03@gmail.com>
2023-11-08 16:16:51 -0800

    exporting firebase as database

--- app/config/firebaseConfig.js
+++ app/config/firebaseConfig.js
@@ -19,4 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export { database };
